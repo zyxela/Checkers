df41360c6b8566d1fc52eab0fb122d0bfc408a8b
FARGO <fargo26sur@mail.ru>
2023-07-25 11:15:05 +0300

    Create repository

--- app/src/main/java/com/example/checkers/View/MainActivity.kt
+++ app/src/main/java/com/example/checkers/View/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.checkers
+package com.example.checkers.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.checkers.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
